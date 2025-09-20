1ec5df23214a2de4e73ea81392a9023a11ff7072
Abel <abel.tewodros-ug@aau.edu.et>
2025-09-20 14:49:45 +0300

    postcss.config.mjs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -44,8 +44,5 @@ export default {
       },
     },
   },
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [],
 };
